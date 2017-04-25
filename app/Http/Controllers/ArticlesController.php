5783e664d3f72c025a5c8b3c8f18f76f4841f0ef
Khon0316 <kimhoon0316@gmail.com>
2017-04-25 16:35:09 +0900

    composer

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -86,8 +86,10 @@ class ArticlesController extends Controller
     public function show($id)
     {
         $article = \App\Article::findOrFail($id);
+        debug($article->toArray());
 
-        return $article->toArray();
+        return view('articles.show', compact('article'));
+        // return $article->toArray();
 
         // return __METHOD__ . '은(는) 다음 기본키를 가진 Article 모델을 조회합니다.:' . $id;
     }
